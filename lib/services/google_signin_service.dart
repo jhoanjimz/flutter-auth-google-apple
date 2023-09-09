2774ade2d4949d817cd3bf2807bd1d2fe3d88339
jhoanjimz <jhoan.developer@gmail.com>
2023-09-09 10:33:27 -0500

    Conectar con backend y validar autenticacion token de Google

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_final_fields
 
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSignInService {
 
@@ -9,11 +10,18 @@ class GoogleSignInService {
   static Future<GoogleSignInAccount?> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? account = await _googleSignIn.signIn();
-      final googleKey = await account!.authentication;
-      print("====================== ID TOKEN ======================");
-      print(googleKey.idToken);
-
-
+      final googleKey = await account?.authentication;
+      final signInWithGoogleEndpoint = Uri(
+        scheme: 'https',
+        host: 'app-google-apple-signin-0f15519ad111.herokuapp.com',
+        path: '/google'
+      );
+      final session = await http.post(
+        signInWithGoogleEndpoint,
+        body: { 'token': googleKey?.idToken }
+      );
+      print("====================== BACKEND ======================");
+      print(session.body);
       return account;
     } catch (e) {
       print('Error en google Signin');
